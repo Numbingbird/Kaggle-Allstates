68a3731a9492dc70abd6cf129990093da1f02d65
Numbingbird <marcohohoho@gmail.com>
2016-11-22 20:19:46 -0500

    split up the data
    matlab starts cutting out signif digits if they're in the same matrix.
    unacceptable loss of data. split up to resolve.

---
+++ src/regLoad.m
@@ -0,0 +1,9 @@
+% Loading takes a long time. Do it once and never again.
+% id cont1	cont2	cont3	cont4	cont5	
+% cont6	cont7	cont8	cont9	cont10	
+% cont11	cont12	cont13	cont14	loss
+
+% files had to be seperated because matlab signifi digits cut them off
+train = csvread('../data/train.csv')
+trainID = csvread('../data/trainID.csv')
+trainY = csvread('../data/trainY.csv')
